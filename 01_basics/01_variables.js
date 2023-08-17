339e32624dedc0c29777395e8839a62f3af7e04b
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-17 15:19:37 +0000

    .

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553;
+let accountEmail = "nilakpathak14@gmail.com"
+var accountPassword = "12345";
+accountCity = "pokhara"
+
+// accountId = 2 not allowed
+
+accountEmail = "nilak@";
+accountPassword = "2222"
+accountCity = "Ktm"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
