6df52429fa6a40de3cba17e6c8b99c1fb81322a9
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-18 04:20:20 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,7 +13,27 @@
 // let booleanIsLoggedIn = Boolean(isloggedIn);
 // console.log(booleanIsLoggedIn)
 
-let number = 33;
-let stringNumber = String(number);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let number = 33;
+// let stringNumber = String(number);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// **************************Operations****************************
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+
+console.log(typeof +true)
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+console.log(num1)
+console.log(num2);
+console.log(num3);
