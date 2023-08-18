4397df558c666b24e7170c2466827df82affcdc7
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-18 16:05:44 +0000

    Destructring

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,8 +1,20 @@
-// const tinderUser = new Object();
-// console.log(tinderUser)
+ // const tinderUser = new Object();
+ // console.log(tinderUser)
 
-const obj1 = {1:"a", 2:"b"};
-const obj2 = {3:"d", 4:"e"};
+// const obj1 = {1:"a", 2:"b"};
+// const obj2 = {3:"d", 4:"e"};
 
-const obj3 = {...obj1 , ...obj2}
-console.log(obj3)
+// const obj3 = {...obj1 , ...obj2}
+// console.log(obj3)
+
+// Destructring 
+
+const course = {
+    coursename: "Js-50Days",
+    price: "999",
+    courseInstructor: "hitesh"
+};
+
+// course.courseInstructor
+const {courseInstructor: i, price: p} = course
+console.log(p);
