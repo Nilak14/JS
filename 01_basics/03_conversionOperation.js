76ffaf299d3763fbb4b20d46252c3adae832d4d7
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-18 04:01:58 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+// let score = '33ab';
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc"=> NaN
+// true => 1; false=>0;
+
+// let isloggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isloggedIn);
+// console.log(booleanIsLoggedIn)
+
+let number = 33;
+let stringNumber = String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
