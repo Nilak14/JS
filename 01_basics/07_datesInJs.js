b97d2d9a6e8c2a93263deb8d7efd017d4aae6e70
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-18 12:57:22 +0000

    dates

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,20 @@
+// dates
+let myDate = new Date();
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+
+// let myCreatedDate = new Date(2023,1,23);
+// console.log(myCreatedDate.toDateString())
+
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp)
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date();
+console.log(newDate.getDay())
