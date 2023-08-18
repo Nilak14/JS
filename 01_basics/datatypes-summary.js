e45a789fa6ee647328ef0c431a419d642cdf9507
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-18 04:51:00 +0000

    tyoeof

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+//***************************Primitive*********************************
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt 
+
+// const id = Symbol('123')
+// const anotherid = Symbol('123')
+// console.log(id === anotherid)
+
+// ********************Reference(Non-primitive)************************
+// Array, Objects, Functions
+
+// const heros = ["IronMan","SpideMan","Captain America"];
+// let myobj = {
+//     name: 'Nilak',
+//     age: 21
+// }
+
+// const myfun = function(){
+//     console.log("Hello World");
+// }
+
+// myfun()
+
+//=============================Type of=================================
+// Number = number
+// string = String
+// boolean = Boolean
+// null = Object
+// undefined = undefined
+// Symbol = Symbol
+// BigInt = BigInt
+
+// arrays = Object
+// object = object
+// function = function
+
+
