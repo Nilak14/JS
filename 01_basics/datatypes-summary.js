e93ca46431f72308044a5a278aee8e8e56450b3c
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-18 07:01:43 +0000

    datatypes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -33,4 +33,22 @@
 // object = object
 // function = function
 
+// --------------------------Stack,Heap---------------------------
+// stack = (Primitve) == returns copy
+// heap = (non-primitive) == returns reference
+// -----------------------------------------------------------------
 
+// let myName = 'Nilak';
+// let anotherName = "Nilak Pathak";
+// anotherName = 'Nirmala'
+// console.log(myName);
+// console.log(anotherName);
+
+let user1 = {
+    email: "nilakpathak14@gmail.com",
+    address: 'Pokhara-08'
+}
+
+let user2 = user1;
+user2.address = 'haha'
+console.log(user1)
