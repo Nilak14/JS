6006851d89e0b2bc6a339bcb7ad50bc4dcd2ef0a
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-18 13:49:48 +0000

    arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,8 @@
+const marvel_heros = ["thor","IronMan","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros)
+// const heros = marvel_heros.concat(dc_heros)
+// console.log(...heros)
+
+console.log(Array.of("Nilak"));
