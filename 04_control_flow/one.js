7c372161a5ba1412f245aeec8d51dac33f2003b9
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-19 11:44:09 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,32 @@
+// if-statement
+// const isUserLoggedIn = true
+// const temperature = 41
+// if(temperature<50){
+//     console.log("less than 50")
+// }
+// else{
+//     console.log("more than 50")
+// }
+
+// <,>,<=,>=
+
+// const balance = 1000;
+// if(balance < 500){
+//     console.log('Less than 500')
+// }
+// else if(balance < 750){
+//     console.log("Less than 750")
+// }
+// else if(balance < 750){
+//     console.log("Less than 750")
+// }
+// else{
+//     console.log("Less than 1200")
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+if(userLoggedIn && debitCard){
+    console.log("Allow do shopping")
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,19 @@
+// const userEmail = ""
+// if (userEmail) {
+//     console.log("Got user email")
+// }else{
+//     console.log("Don't have user email")
+// }
+
+// falsy value
+// false, 0, -0, 0n, "", null, undefined, NaN; 
+
+// tricky truthy value
+// "0", "false", " ", [], {}, function(){}
+
+// Nullish coalescing Operator(??): null,undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 20
+console.log(val1)
