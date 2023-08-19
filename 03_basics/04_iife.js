72f1e8724773a7124153bb73cadc2134ce27d960
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-19 11:01:47 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,9 +1,8 @@
 // Immediately Invoked Function Expression (IIFE)
-
 (function chai(){
     console.log("DB CONNECTED")
 })();
 
 ( (name)=>{
     console.log(`DB CONNECTED TWO ${name}`)
-})('nilak')
+})('NILAK');   
