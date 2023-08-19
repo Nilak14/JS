96d132bb0619a591c6a2a43fb2cac6616798b693
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-19 05:20:54 +0000

    hositing

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,9 +1,31 @@
-let a = 300
-if(true){
-     let a = 10
-    const b = 20
-    var c = 30
-    console.log(a)
+// let a = 300
+// if(true){
+//      let a = 10
+//     const b = 20
+//     var c = 30
+//     console.log(a)
+// }
+
+// console.log(a)
+
+// function one(){
+//     const userName = "Nilak";
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(userName)
+//     }
+//     console.log(website);
+//     two()
+// }
+// one()
+
+addOne(5)
+function addOne(num){
+    return num+1;
 }
 
-console.log(a)
+addTwo(5);
+const addTwo = function(num){
+    return num+2
+}
