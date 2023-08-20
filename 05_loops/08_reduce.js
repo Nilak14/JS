11b4358dc64afd79f2f9505d049e271caa60802a
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-20 11:19:10 +0000

    dom

--- 05_loops/08_reduce.js
+++ 05_loops/08_reduce.js
@@ -1,29 +1,31 @@
-const myNums = [1,2,3];
-const myTotoal = myNums.reduce((acc,currentVal)=>{
-    // console.log(`acc: ${acc} and curr: ${currentVal}`)
-    return acc + currentVal
-},0 /*Initial accemulator value */)
+// const myNums = [1,2,3];
+// const myTotoal = myNums.reduce((acc,currentVal)=>{
+//     // console.log(`acc: ${acc} and curr: ${currentVal}`)
+//     return acc + currentVal
+// },0 /*Initial accemulator value */)
 
-// console.log(myTotoal)
+// // console.log(myTotoal)
 
-const shoopingCart = [
-    {
-        itemName: "Shirt",
-        price: 500
-    },
-    {
-        itemName: "Pant",
-        price: 800
-    },
-    {
-        itemName: "Coat",
-        price: 1500
-    },
-    {
-        itemName: "Shoes",
-        price: 3500
-    },
-]
+// const shoopingCart = [
+//     {
+//         itemName: "Shirt",
+//         price: 500
+//     },
+//     {
+//         itemName: "Pant",
+//         price: 800
+//     },
+//     {
+//         itemName: "Coat",
+//         price: 1500
+//     },
+//     {
+//         itemName: "Shoes",
+//         price: 3500
+//     },
+// ]
 
-const total = shoopingCart.reduce((acc,item)=> (acc+item.price),0);
-console.log(total)
+// const total = shoopingCart.reduce((acc,item)=> (acc+item.price),0);
+// console.log(total)
+
+
