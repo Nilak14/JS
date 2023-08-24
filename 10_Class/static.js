641357fe61b56e416dc1bb31f908005ef61a143c
Nilak Pathak <117818016+Nilak14@users.noreply.github.com>
2023-08-24 13:50:00 +0000

    STATIC

---
+++ 10_Class/static.js
@@ -0,0 +1,27 @@
+class User{
+    constructor(username){
+        this.username = username;
+    }
+
+    logMe(){
+        console.log(`UserName: ${this.username}`)
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const nilak = new User("nilak")
+// console.log(nilak.createId())
+
+class Teacher extends User{
+    constructor(username,email){
+        super(username)
+        this.email = email
+
+    }
+}
+
+const iphone = new Teacher("Iphone","iphone@apple.com")
+console.log(iphone);
